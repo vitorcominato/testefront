c52aebbb01e16aed3c4190b443864ac1eff3090d
Vitor Cominato <vitor.cominato@gmail.com>
2016-03-21 00:15:26 -0300

    Page Complete

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -13,5 +13,49 @@ $(function() {
         locale: 'pt-br',
         minDate: new Date()
     });
+
+    var t;
+	$(".sideMenu").mouseenter(function(){
+		if($(".sideMenu").width() < 190 && !$(this).hasClass("open")){
+			$(".itemMenu").css({
+				"height": "auto",
+				"float": "left",
+				"width": "100%",
+				"margin": "auto"
+		    });
+		    $(".sideMenu").clearQueue();
+			$(".sideMenu").animate({width: '220px'},300, function(){
+				$(".nameMenu").fadeIn();
+				$(this).addClass("open");
+			});
+			t = true;
+			setTimeout(function () {
+	            t = false;
+	        }, 1000);
+		}
+	}).mouseleave(function(){
+		if($(this).hasClass("open") && !t){
+			$(".nameMenu").clearQueue();
+		    $(".nameMenu").fadeOut(300, function(){
+		    	$(".sideMenu").animate({width: '8%'},200);
+		    	$(".itemMenu").css({
+					"float": "none",
+				    "height": "60px",
+				    "width": "30px",
+				    "margin": "auto"
+			    });
+			    $(".sideMenu").removeClass("open");
+		    });
+		}
+	});
+	$('.profileBox').popover({
+		animation: 	true,
+		html: 		true,
+		container: 	"body",
+		placement: 	"bottom", 
+		content: 	$(".popoverContent").html()
+	});
+
+	$('#example').popover();
     
 });
